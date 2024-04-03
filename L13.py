f06c66b367160cbcd2464a07565ad4e2537f5400
Cory Codling <ccodling@navex.com>
2024-04-03 10:11:06 -0600

    Lesson 13 example

---
+++ L13.py
@@ -0,0 +1,14 @@
+#determine if a number is prime
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter a number: "))
+if is_prime(num):
+    print(f"{num} is a prime number")
+else:
+    print(f"{num} is not a prime number")
